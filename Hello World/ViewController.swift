e84cd525971eaae6090cc7d3e027d5a8b7140ca2
Philip Ross <rosspa@rose-hulman.edu>
2015-02-01 23:37:55 -0500

    initial commit

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var count = 0
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    @IBAction func hitButton(sender: AnyObject) {
+        count++
+        label.text = "count = \(count)"
+    }
 
 }
 
